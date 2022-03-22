7964023ab2fa1c631124fd06bfd210eb62272086
Jackson Wray <jackson.wray19@outlook.com>
2022-03-22 20:40:08 +0800

    routes for homepage/login, auth written

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get homepage handlebar, blogs, navbar and login.
+
+router.get('/', withAuth, (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('dashboard', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
