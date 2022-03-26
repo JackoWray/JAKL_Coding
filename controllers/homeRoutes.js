a6ce475da2eff61c5c8494d11a4b31f166c45f40
Jackson Wray <jackson.wray19@outlook.com>
2022-03-26 13:55:38 +0800

    Added signup endpoint on homeRoutes and userRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,13 +4,13 @@ const withAuth = require('../helpers/auth');
 
 // Get homepage handlebar, blogs, navbar and login.
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const users = userData.map((user) => user.get({ plain: true }));
 
     res.render('dashboard', {
       users,
@@ -21,13 +21,22 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
