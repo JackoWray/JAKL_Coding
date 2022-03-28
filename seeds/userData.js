3d8aabbbad39ccc5633f127459b48da63733dec8
kbario <kylebario1@gmail.com>
2022-03-28 21:16:31 +0800

    updated passwords in seed data

--- seeds/userData.js
+++ seeds/userData.js
@@ -5,31 +5,31 @@ const userData = [
     first_name: 'Liz',
     last_name: 'Mackle',
     email: 'liz.mackle@hotmail.com',
-    password: 'password12345',
+    password: '$2b$10$wrcLlpEATEmPyP.TeqRxHevObnNquSQBc4Otrw0AEaUDQImkphEPq',
   },
   {
     first_name: 'Kyle',
     last_name: 'Bario',
     email: 'kyle.bario@hotmail.com',
-    password: 'password12345',
+    password: '$2b$10$wrcLlpEATEmPyP.TeqRxHevObnNquSQBc4Otrw0AEaUDQImkphEPq',
   },
   {
     first_name: 'Jackson',
     last_name: 'Wray',
     email: 'jackson_wrayhotmail.com',
-    password: 'password12345',
+    password: '$2b$10$wrcLlpEATEmPyP.TeqRxHevObnNquSQBc4Otrw0AEaUDQImkphEPq',
   },
   {
     first_name: 'Jane',
     last_name: 'Stephenson',
     email: 'jane-stephenson@hotmail.com',
-    password: 'password12345',
+    password: '$2b$10$wrcLlpEATEmPyP.TeqRxHevObnNquSQBc4Otrw0AEaUDQImkphEPq',
   },
   {
     first_name: 'John',
     last_name: 'Jones',
     email: 'john_jones@hotmail.com',
-    password: 'password12345',
+    password: '$2b$10$wrcLlpEATEmPyP.TeqRxHevObnNquSQBc4Otrw0AEaUDQImkphEPq',
   },
 ];
 
