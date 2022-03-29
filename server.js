5b68232a096e0e9b78398cc3cedef5a8f74f4c17
LizMackle <liz.mackle@outlook.com>
2022-03-29 17:56:03 +0800

    mock up images added

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening to http://localhost:3001'));
 });
